44ecb3a611881dbc4bce34abd480d07b4cc56524
Kayky Renner Siqueira Galindo <kayky.renner.sg@gmail.com>
2025-02-14 16:35:56 -0300

    knex

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./server/app");
 const PORT = process.env.PORT || 3100; // Corrigido o nome da variável de ambiente
-const {dbLocal} = require("./server/database/bancoDeDados/database");
+const db = require("./server/database/bancoDeDados/database");
 const { tabelasController } = require("./server/database/indexTables");
 
 const startServer = async () => {
@@ -8,14 +8,14 @@ const startServer = async () => {
         console.log("Inicializando o banco de dados...");
 
         // Criar tabela de usuários primeiro
-        const tabelaUsuarioCriada = await tabelasController.criaTabelaUsu(dbLocal);
+        const tabelaUsuarioCriada = await tabelasController.criaTabelaUsu(db);
         
         // Se a tabela 'usuarios' foi criada (ou já existia), prosseguir com as outras
         if (tabelaUsuarioCriada) {
             console.log("Tabela 'usuarios' criada. Criando as demais tabelas...");
-            await tabelasController.criarTabelaBarbeiro(dbLocal);
-            await tabelasController.createTabelaCliente(dbLocal);
-            await tabelasController.criaTabelaHorario(dbLocal);
+            await tabelasController.criarTabelaBarbeiro(db);
+            await tabelasController.createTabelaCliente(db);
+            await tabelasController.criaTabelaHorario(db);
         } else {
             console.log("Erro ao criar a tabela 'usuarios'. Outras tabelas não serão criadas.");
             return; // Encerra a função caso a criação falhe
--- src/server/database/bancoDeDados/database.js
+++ src/server/database/bancoDeDados/database.js
@@ -1,15 +1,17 @@
 const knex = require('knex'); 
 // Configuração do Knex para o MySQL 
-const dbLocal = knex({ 
+const db = knex({ 
     client: 'mysql2', 
     connection: { 
         host: process.env.DB_HOST, 
         user: process.env.DB_USER, 
         password: process.env.DB_PASSWORD, 
-        database: process.env.DB_NAME, 
+        database: process.env.DB_NAME,
+        port:process.env.DB_PORT
+        
     }, 
 });
 
 
 
-module.exports = {dbLocal}
+module.exports = db
