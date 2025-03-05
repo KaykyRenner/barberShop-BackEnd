1702eef72270888aec3029734dd7246db0695cfe
Kayky Renner Siqueira Galindo <kayky.renner.sg@gmail.com>
2025-03-05 11:13:36 -0300

    REDME

--- src/server/app.js
+++ src/server/app.js
@@ -5,7 +5,10 @@ const cors = require('cors');
 const bodyParser= require('body-parser'); 
 const router = require('./routes/router'); 
 const jtw = require('jsonwebtoken') 
-const app = express(); 
+const app = express();
+app.use(cors({
+    origin: process.env.CORS
+}))
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended:true})); 
 app.use(router); 
